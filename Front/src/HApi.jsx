7d0559bd6e40dffccaa0c32645c8a4ec4a4fb557
dipatzlaff <114265802+dipatzlaff@users.noreply.github.com>
2024-04-18 22:00:25 -0300

    Update HApi.jsx

--- Front/src/HApi.jsx
+++ Front/src/HApi.jsx
@@ -3,7 +3,7 @@ import './Home.css'
 import { Link } from 'react-router-dom'
 
 
-function HApi() {
+function HEpi() {
 
 
   return (
@@ -11,8 +11,17 @@ function HApi() {
       <Link to={"/"}>
           < h1 > Historico de Epis</h1>
         </Link >
+        <div>
+        <h3>ID Epi's</h3>
+        <input type='number' id="BuscarHistoricoEpi" onChange={(evento) => setDevolucao(evento.target.value)}/>
+        </div>
+        <button className='BuscarHistoricoEpi'>
+          <span>Buscar</span>
+          </button>
+        <div className='todo'></div>
+        
       </div>
   )
 }
 
-export default HApi
+export default HEpi
