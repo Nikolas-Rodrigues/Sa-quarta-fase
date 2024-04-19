df26df364f533c797ce18ee067fd10ee30951890
Nikolas-Rodrigues <cruz.nikolasr@gmail.com>
2024-04-18 21:16:07 -0300

    Correção de Bugs e sistema de verificação

--- Front/src/Epis.jsx
+++ Front/src/Epis.jsx
@@ -243,10 +243,10 @@ function Epi() {
               {epi.map(epi => (
                 <li key={epi.id}>
                   <div className='organizar1'>
-                    <div className='epiId'>{epi.id}</div>
-                    <div className='epiNome'>{epi.nome}</div>
-                    <div className='epiValidade'>{epi.validade}</div>
-                    <div className='epiDisponibilidade'>{epi.disponibilidade}</div>
+                    <div className='epiCenter'>{epi.id} </div>
+                    <div className='epiCenter'>{epi.nome}</div>
+                    <div className='epiCenter'>{epi.validade}</div>
+                    <div className='epiCenter'>{epi.disponibilidade}</div>
                   </div>
                 </li>
               ))}
